b79ff109af0f7c7e70714ddd2ec016f1d3d62c3d
Kisters <kistters@it>
2018-06-27 22:39:48 -0300

    add swarm suport :D

---
+++ start-up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker rm -f $(docker ps -a -q)
+
+if ! docker node ls > /dev/null 2>&1; then
+   docker swarm init
+fi
+ 
+docker build -t devops-php7-alpine:latest ./php/php-fpm-7-alpine
+docker build -t devops-php5.6-alpine:latest ./php/php-fpm-5.6-alpine
+
+docker stack deploy -c DevOps-Nginx-Php-MySQL.yml DevOpsNginxPhpMySQL
