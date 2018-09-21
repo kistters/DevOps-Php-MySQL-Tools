970642c231d48d131cffb55f781a4e300e1140d6
MauMau <rafael.kisters@fcamara.com.br>
2018-09-21 15:15:57 -0300

    network on php-5

--- start-up.sh
+++ start-up.sh
@@ -10,3 +10,8 @@ docker build -t devops-php7-alpine:latest ./php/php-fpm-7-alpine
 docker build -t devops-php5.6-alpine:latest ./php/php-fpm-5.6-alpine
 
 docker stack deploy -c DevOps-Nginx-Php-MySQL.yml DevOpsNginxPhpMySQL
+
+# validate
+#docker-compose -f DevOps-Nginx-Php-MySQL.yml up
+#docker stack services DevOpsNginxPhpMySQL
+#docker stack rm DevOpsNginxPhpMySQL
